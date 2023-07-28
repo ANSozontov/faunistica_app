e7e84f8f60962cb41f1fccfecc6ea9bc84c83bcb
ANSozontov <A.N.Sozontov@gmail.com>
2023-07-28 11:46:05 +0500

    1213

--- app_psql.connection.R
+++ app_psql.connection.R
@@ -25,68 +25,73 @@ ui <- fluidPage(
                 uiOutput("little_title"),
                 HTML("<br>"),
                 tableOutput("res_table"),
-                HTML("<br>"),
-                sliderInput("bins", 
-                            "Number of bins:",
-                            min = 1,
-                            max = 50,
-                            value = 30),
-                plotOutput("distPlot")
+                HTML("<br>")
             )
             )
             ),
-            tabPanel("Ввод новых чаепитий")
+            tabPanel("Ввод новых чаепитий", 
+            sidebarLayout(
+            sidebarPanel(
+                HTML("<br>"), 
+                textInput("i_name1", "Чаёвник1"),
+                textInput("i_name2", "Чаёвник2"),
+                dateInput("i_dat", "Дата чаепития", format = "yyyy/mm/dd"),
+                textInput("i_proof", "Я видел это своими глазами и подтверждаю факт чаепития"),
+                HTML("<br>"),
+                actionButton("record", "Записать!")
+            ), 
+            mainPanel(
+                tags$img(src = "dog.jpeg", width = 500)
+            )
+            )
+            )
         )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 # Connect -----------------------------------------------------------------
-    con_config <- list(database = "rdatabase", 
+    con_config <- list(database = "rdatabase",
                        hostname = "localhost",
-                       dsn_port = "5432", 
-                       dsn_uid  = "ruser", 
-                       mypass   = scan("/var/mypass", 
+                       dsn_port = "5432",
+                       dsn_uid  = "ruser",
+                       mypass   = scan("/var/mypass",
                             what = "", nlines = 1, quiet = TRUE))
-    con <- dbConnect(dbDriver("PostgreSQL"), 
+    con <- dbConnect(dbDriver("PostgreSQL"),
                      dbname = con_config$database,
-                     host = con_config$hostname, 
+                     host = con_config$hostname,
                      port = con_config$dsn_port,
-                     user = con_config$dsn_uid, 
+                     user = con_config$dsn_uid,
                      password = con_config$mypass)
 
 # Prepare table -----------------------------------------------------------
-    testdf <- data.frame(name1 = c("Petir", "Mike", "Norman", "Borman", "Xi", "Xi"), 
-               name2 = c("Mike", "Anna", "Borman", "Norman", "Petir", "Anna"), 
-               dat = as.Date(c("02/12/22", "12/02/23", "01/10/21", 
-                               "01/01/00", "26/07/89", "26/07/23"), 
-                             format = "%d/%m/%y"), 
-               proof = c("Jon", "John", "Johan", "Joe", "Ju", "Go"))
+    # testdf <- data.frame(name1 = c("Petir", "Mike", "Norman", "Borman", "Xi", "Xi"),
+    #            name2 = c("Anna", "Anna", "Borman", "Norman", "Petir", "Anna"),
+    #            dat = as.Date(c("02/12/22", "12/02/23", "01/10/21",
+    #                            "01/01/00", "26/07/89", "26/07/23"),
+    #                          format = "%d/%m/%y"),
+    #            proof = c("Jon", "John", "Johan", "Joe", "Ju", "Go"))
     
-    users <- reactive({unique(c(testdf$name1, testdf$name2))})
+    users <- reactive({unique(c(
+        DBI::dbGetQuery(con,"SELECT * FROM my_table")$name1, 
+        DBI::dbGetQuery(con,"SELECT * FROM my_table")$name2))})
     
     output$names_selector <- renderUI({
         selectInput("usr", "Чаёвник", choices = users()) 
     })
-    
-    output$little_title <- renderUI({paste0("Результаты поиска чаепитий для ", 
-                                          input$usr,
-                                          ":")})
 
-# Dummy -------------------------------------------------------------------
-    output$distPlot <- renderPlot({
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    name_toshow <- eventReactive(input$refresh, {input$usr})
+    observe({
+        input$refresh
+        output$little_title <- renderUI({paste0("Результат. Для ", 
+                                                name_toshow(),
+                                                " найдены следующие чаепития:")})
     })
     
-    
-    
-    observeEvent(input$refresh, {
-        df <- reactive({
-            testdf %>% 
+    # observeEvent(input$refresh, {
+        df <- eventReactive(
+            input$refresh, {
+            DBI::dbGetQuery(con,"SELECT * FROM my_table") %>% 
                 filter(name1 == input$usr | name2 == input$usr) %>% 
                 transmute(
                     `С кем:` = case_when(name1 == input$usr ~ name2, TRUE ~ name1), 
@@ -94,8 +99,32 @@ server <- function(input, output) {
                     `Подтверждает:` = proof)
         })
         
-        output$res_table <- renderTable({df()})
-    })
+        output$res_table <- renderTable({slice(df(), 1:input$rowstoshow)})
+        
+        observeEvent(input$refresh, {
+            updateNumericInput(session, inputId = "rowstoshow", 
+                               max = nrow(df()), value = nrow(df()))
+        })
+        
+        
+        
+        observeEvent(input$record, {
+            i_last <- data.frame(name1 = input$i_name1, name2 = input$i_name2, 
+                       dat = str_replace_all(as.character(input$i_dat), "-", "/"), 
+                       proof = input$i_proof)
+            i_succ <- DBI::dbWriteTable(con, "my_table", i_last, 
+                                  append = TRUE, row.names = FALSE)
+            if(i_succ){
+                showNotification("Записано благополучно!", type = "message")
+            } else {
+                showNotification("Что-то не благополучно...", type = "error")
+            }
+            updateTextInput(session, inputId = "i_name1", value = "")
+            updateTextInput(session, inputId = "i_name2", value = "")
+            updateDateInput(session, inputId = "i_dat", value = NULL)
+            updateTextInput(session, inputId = "i_proof", value = "")
+        })
+
 }
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server, options = list(port = 4321))
